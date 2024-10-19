7737421b0300a6455b590f331358253bb3e7897d
Wirachit Panasit <51145476+avadakedavra-wp@users.noreply.github.com>
2024-10-19 14:54:20 +0700

    Update Footer.tsx

--- frontend/poc-app/app/components/Footer/Footer.tsx
+++ frontend/poc-app/app/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Footer: React.FC = () => {
+const Footer = () => {
     return (
         <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <div>
