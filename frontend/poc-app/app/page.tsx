1150ea7dc286d06378421c5832b9ad3849ce13ed
NewDev <65420147@kmitl.ac.th>
2024-10-27 11:04:13 +0700

    Remove unused

--- frontend/poc-app/app/page.tsx
+++ frontend/poc-app/app/page.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import Typewriter from 'typewriter-effect';
-import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
--- frontend/poc-app/app/welcome/page.tsx
+++ frontend/poc-app/app/welcome/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from 'react';
 import Typewriter from 'typewriter-effect';
 import Image from 'next/image';
 import { FAQItem } from './_components/faq-item';
